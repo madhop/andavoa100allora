e5b33807ce79ea124cc98e0b677a45a398491c83
Luca <luca.fucci@outlook.com>
2018-11-08 10:52:04 +0100

    prova

---
+++ prova.m
@@ -0,0 +1,26 @@
+% PERVASIVE
+close all
+clear
+clc
+
+
+% tempo, x, y, z
+% tempo, latitudine, longitudine
+%% read data
+data = readtable('104107.csv');
+gps_data = [];
+acc_data = [];
+
+for i=1:size(data,1)
+    if strcmp(data{i,1}, 'ACC')
+        acc_data = [acc_data; data(i,2:8)];
+    else
+        gps_data = [gps_data; data(i,2:4)];
+    end
+end
+
+acc_data = table2array(acc_data);
+gps_data = table2array(gps_data);
+%% 
+plot( acc_data(:,4));
+lowpass(acc_data(:,4),0.001)
