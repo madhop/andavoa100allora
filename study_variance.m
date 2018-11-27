37d90fc5dea048e0cf1793eb877c193ef350471d
Umberto <umbertofazio92@gmail.com>
2018-11-27 10:27:17 +0100

    Update study_variance.m

--- study_variance.m
+++ study_variance.m
@@ -7,7 +7,7 @@ clc
 % tempo, x, y, z
 % tempo, latitudine, longitudine
 %% read data
-data = readtable('f211118.csv'); %104107 %162736 %110122 %160208 %16111417 %17111738    %f144015 %132038
+data = readtable('long221118.csv'); %f211118
 gps_data = [];
 acc_data = [];
 
@@ -31,19 +31,6 @@ plot(acc_data(:,1), acc_data(:,4));
 
 %% SCINDERE X,Y,Z
 
-%% FFT
-Fs = 1000;
-L = 10;
-Y = fft(acc_data(100:100+L,3));
-P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-
-f = Fs*(0:(L/2))/L;
-plot(f,P1)
-title('Single-Sided Amplitude Spectrum of X(t)')
-xlabel('f (Hz)')
-ylabel('|P1(f)|')
 
 %% VARIANCE
 height = mean(acc_data(:,4));
@@ -81,28 +68,45 @@ asphalt = iddata(acc_data(891:1833,4),[],0.068);
 % end
 % split_asphalt{i+2} = asphalt((i+1)*d + 1:end);
 
-split_asphalt{1} = asphalt(1:100);
-split_asphalt{2} = asphalt(101:200);
-split_asphalt{3} = asphalt(201:300);
-split_asphalt{4} = asphalt(301:400);
-split_asphalt{5} = asphalt(401:500);
-split_asphalt{6} = asphalt(501:600);
-split_asphalt{7} = asphalt(601:700);
-split_asphalt{8} = asphalt(701:800);
+d1 = asphalt(1:250);
+d2 = asphalt(251:530);
+d3 = asphalt(531:880);
 
-
+% split_asphalt{4} = asphalt(301:400);
+% split_asphalt{5} = asphalt(401:500);
+% split_asphalt{6} = asphalt(501:600);
+% split_asphalt{7} = asphalt(601:700);
+% split_asphalt{8} = asphalt(701:800);
 
 % merge
-clear d
-d = merge(split_asphalt{1},split_asphalt{2},split_asphalt{3}, split_asphalt{4},split_asphalt{5}, split_asphalt{6}, split_asphalt{7}, split_asphalt{8});
+% d = merge(split_asphalt{1},split_asphalt{2},split_asphalt{3}, split_asphalt{4},split_asphalt{5}, split_asphalt{6}, split_asphalt{7}, split_asphalt{8});
+d = merge(d1,d2,d3)
 
-de = getexp(d,[1,2,3,4,5,6,7]);
-dv = getexp(d,8);
+d.exp = {'Period 1';'Day 2';'Phase 3'}
+de = getexp(d,[1,2]);      % subselection is done using  the command GETEXP 
+dv = getexp(d,'Phase 3');  % using numbers or names.
 
 %% MODEL 
-model1 = armax(de, [4 4]);
+model1 = armax(de,[4 4]);
 compare(dv, model1)
 
 %% ARMA MODEL
-data1 = iddata(acc_data(:,4),[],0.068);
-arma1 = armax(data1,[4 4]);
+% data1 = iddata(acc_data(:,4),[],0.068);
+arma1 = armax(asphalt,[5 5]);
+compare(asphalt, arma1)
+
+%% SEPARIAMOLI TUTTI
+autob = acc_data(find(acc_data(:,1)>= 14767159 & acc_data(:,1)<= 14868275), :);
+bosco1 = acc_data(find(acc_data(:,1)>= 14934292 & acc_data(:,1)<= 15115430), :);
+asphalt = acc_data(find(acc_data(:,1)>= 15210511 & acc_data(:,1)<= 15262562), :);
+roto1 = acc_data(find(acc_data(:,1)>= 15380649 & acc_data(:,1)<= 15431687), :);
+prato = acc_data(find(acc_data(:,1)>= 15453539 & acc_data(:,1)<= 15501570), :);
+piastra1 = acc_data(find(acc_data(:,1)>= 15559780 & acc_data(:,1)<= 15583643), :);
+sterra = acc_data(find(acc_data(:,1)>= 15608653 & acc_data(:,1)<= 15624877), :);
+piastra2 = acc_data(find(acc_data(:,1)>= 15633668 & acc_data(:,1)<= 15679876), :);
+roto2 = acc_data(find(acc_data(:,1)>= 15731754 & acc_data(:,1)<= 15786963), :);
+bosco2 = acc_data(find(acc_data(:,1)>= 16012153), :);
+
+%%
+
+hist(bosco2(:,4),100);
